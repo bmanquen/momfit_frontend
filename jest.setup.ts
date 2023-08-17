4f6dacb05331a2ba4eb252f7a9c3c0be1cbf2967
Brenden Manquen <bmanquen1993@gmail.com>
2023-08-17 15:20:03 +0000

    Root Layout

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom";
+import React from "react";
+
+// Workaround for nextjs Image
+function MockImage(props) {
+  return React.createElement("img", props);
+}
+jest.mock("next/image", () => MockImage);

b8473353fb381488331a425779caa5b693a5a3d7
Brenden Manquen <bmanquen1993@gmail.com>
2023-08-17 15:52:45 +0000

    Home Page Hero Section

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,9 +1,35 @@
 import Home from "./page";
 import React from "react";
-import { render } from "../utils/test-utils";
+import { render, screen } from "../utils/test-utils";
 
 describe("Home Page, ", () => {
   it("renders", () => {
     render(<Home />);
+
+    // Hero Image
+    expect(
+      screen.getByRole("heading", { name: /brooke's business/i })
+    ).toBeVisible();
+
+    // Services
+    expect(screen.getByRole("heading", { name: /services/i })).toBeVisible();
+    expect(
+      screen.getByRole("heading", { name: /pre-pregnancy/i })
+    ).toBeVisible();
+    expect(
+      screen.getByRole("heading", { name: /post-pregnancy/i })
+    ).toBeVisible();
+    expect(screen.getByRole("heading", { name: /^pregnancy$/i })).toBeVisible();
+    expect(screen.getByText(/we offer pre-pregnancy services/i)).toBeVisible();
+    expect(screen.getByText(/we offer post-pregnancy services/i)).toBeVisible();
+    expect(screen.getByText(/we offer pregnancy services/i)).toBeVisible();
+
+    // About
+    expect(screen.getByRole("heading", { name: /about us/i })).toBeVisible();
+    expect(
+      screen.getByText(
+        /we specialize in pelvic floor corrective training to either help you prepare for pregnancy or to help you after pregnancy/i
+      )
+    ).toBeVisible();
   });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "../styles/page.module.css";
 import { Typography } from "@mui/material";
 import React from "react";
@@ -6,96 +5,57 @@ import React from "react";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className="bg-[url('../../public/heroImage.png')] bg-cover bg-no-repeat w-screen py-72">
-        <Typography className="text-center" variant="h1" fontWeight={800}>
+      <div className="relative w-screen mb-16">
+        <div className="bg-[url(../../public/heroImage.png)] bg-cover py-80">
+          <div className="absolute bg-black opacity-50 inset-0 h-full w-full" />
+        </div>
+        <Typography
+          className="text-center absolute inset-0 h-full w-full top-1/3"
+          variant="h1"
+          fontWeight={800}
+        >
           Brooke's Business
         </Typography>
       </div>
-      <div className={styles.description}>
-        <p>
-          gs Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="w-[70%]">
+        <Typography borderBottom={1} className="mb-6" variant="h4">
+          Services
+        </Typography>
+        <div className="flex justify-between pb-16">
+          <div>
+            <Typography borderBottom={1} className="mb-2" variant="h5">
+              Pre-Pregnancy
+            </Typography>
+            <Typography variant="body1">
+              We offer pre-pregnancy services.
+            </Typography>
+          </div>
+          <div>
+            <Typography borderBottom={1} className="mb-2" variant="h5">
+              Pregnancy
+            </Typography>
+            <Typography variant="body1">
+              We offer pregnancy services.
+            </Typography>
+          </div>
+          <div>
+            <Typography borderBottom={1} className="mb-2" variant="h5">
+              Post-Pregnancy
+            </Typography>
+            <Typography variant="body1">
+              We offer post-pregnancy services.
+            </Typography>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div>
+          <Typography borderBottom={1} className="mb-6" variant="h4">
+            About Us
+          </Typography>
+          <Typography variant="body1">
+            We specialize in pelvic floor corrective training to either help you
+            prepare for pregnancy or to help you after pregnancy.
+          </Typography>
+        </div>
       </div>
     </main>
   );

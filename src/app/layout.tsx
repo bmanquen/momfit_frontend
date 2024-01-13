ba4a84fe70c122cde4ea308c09b66c2da651e02c
Brenden Manquen <bmanquen1993@gmail.com>
2024-01-13 12:32:22 +0000

    Add Navigation Bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,16 @@ import "../styles/globals.css";
 import { Inter, Montserrat, Averia_Serif_Libre } from "next/font/google";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 // TODO: Implement Analytics once we resolve issues with Jest.
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { AppBar, Button, Toolbar } from "@mui/material";
+import { AppBar, Button, IconButton, Toolbar } from "@mui/material";
+import { isMobileOnly } from "react-device-detect";
+import logo from "../../public/logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import LaunchIcon from "@mui/icons-material/Launch";
+import clsx from "clsx";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -30,14 +36,25 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [activeNavItem, SetActiveNavItem] = React.useState(0);
-  const pages = ["Home", "Exercises", "Blog"];
-  const links = ["/", "/exercises", "/blog"];
+  const [activeNavItem, setActiveNavItem] = React.useState(0);
+  const [isNavOpen, setIsNavOpen] = React.useState(false);
+  const [isMobileDevice, setIsMobileDevice] = React.useState(false);
+  const pages = ["Home", "Events"];
+  const links = ["/", ""];
 
-  // TODO: Implement Nav Bar when adding pages to site.
-  // const navChange = (index: number) => {
-  //   SetActiveNavItem(index);
-  // };
+  React.useEffect(() => {
+    if (isMobileOnly) {
+      setIsMobileDevice(true);
+    }
+  });
+
+  const navChange = (index: number) => {
+    setActiveNavItem(index);
+  };
+
+  function toggleNav() {
+    setIsNavOpen(!isNavOpen);
+  }
   return (
     <html
       lang="en"
@@ -70,39 +87,71 @@ export default function RootLayout({
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
-        {/* <AppBar className="bg-white" component="nav">
+        <AppBar color="inherit" component="nav">
           <Toolbar>
-            <div className="flex justify-around w-full">
-              {pages.map((page, index) => {
-                return (
-                  <div
-                    className={
-                      activeNavItem === index
-                        ? "border-b-4 border-black border-solid"
-                        : ""
-                    }
-                    data-testid={`navItem${index}`}
-                    key={index}
-                  >
-                    <Button
-                      className="text-black hover:bg-slate-200 md:py-4 font-bold"
-                      onClick={() => navChange(index)}
-                      variant="text"
-                    >
-                      <Link href={links[index]}>{page}</Link>
-                    </Button>
-                  </div>
-                );
-              })}
+            <div className="flex flex-col text-center md:flex-row w-full">
+              {isMobileDevice ? (
+                <IconButton
+                  className="text-left self-start"
+                  onClick={toggleNav}
+                >
+                  <MenuIcon />
+                </IconButton>
+              ) : null}
+              {!isMobileDevice ? (
+                <Link href={links[0]}>
+                  <Image
+                    alt="Momfit logo"
+                    className="filter invert mr-6"
+                    src={logo}
+                    width={60}
+                  />
+                </Link>
+              ) : null}
+              {(isMobileDevice && isNavOpen) || !isMobileDevice
+                ? pages.map((page, index) => {
+                    return (
+                      <div data-testid={`navItem${index}`} key={index}>
+                        <Button
+                          className={clsx([
+                            activeNavItem === index ? "bg-gray-100" : "",
+                            "text-black hover:bg-gray-100 w-full md:w-auto h-full md:mx-2 md:px-4 rounded-none",
+                          ])}
+                          onClick={() => navChange(index)}
+                          variant="text"
+                        >
+                          <Link
+                            className="font-montserrat tracking-widest font-bold"
+                            href={
+                              page === "Events"
+                                ? "https://lvoaet.pushpress.com/landing/events/cal-c86af468-d5d3-4eed-b7c8-5080"
+                                : links[index]
+                            }
+                            target="_blank"
+                          >
+                            {page}
+                            {page === "Events" ? (
+                              <LaunchIcon
+                                className="ml-2"
+                                color="disabled"
+                                fontSize="small"
+                              />
+                            ) : null}
+                          </Link>
+                        </Button>
+                      </div>
+                    );
+                  })
+                : null}
             </div>
           </Toolbar>
-        </AppBar> */}
+        </AppBar>
         <div>
-          {/* <Toolbar /> */}
+          <Toolbar />
           {children}
-          <Analytics />
-          <SpeedInsights />
         </div>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );

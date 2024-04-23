2766faa2ceac4a510bcfd0fe2a0a705f9b64e857
Brenden Manquen <bmanquen1993@gmail.com>
2024-04-23 13:32:37 +0000

    Optimize queries

--- src/hooks/queries/useGetEvents.ts
+++ src/hooks/queries/useGetEvents.ts
@@ -2,11 +2,16 @@ import { momfitBackend } from "@/src/utils/constants";
 import { useQuery } from "@tanstack/react-query";
 
 const fetchEvents = async () => {
-  const mfEvents: Array<MF_Event> = await (
-    await fetch(`${momfitBackend}/events`)
-  ).json();
-
-  return mfEvents;
+  try {
+    const response = await fetch(`${momfitBackend}/events`);
+    if (!response.ok) {
+      throw new Error("Failed to get events");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    throw error;
+  }
 };
 
 export const useGetEvents = () => {
